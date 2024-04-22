56121c8b0613c1977bf9485d8e76ab9dedd3a5e9
yagubhajili <yaqubhacili6@gmail.com>
2024-04-22 12:35:26 +0400

    ecommerce api

---
+++ 20-EcommerceWithJsonServerApi/assets/js/checkAdmin.js
@@ -0,0 +1,14 @@
+let AdminPanel = document.querySelector(".AdminPanel")
+
+if (JSON.parse(localStorage.getItem("isAdmin"))) {
+        AdminPanel.style.display = "block"
+    } else {
+        AdminPanel.style.display = "none"
+    }
+
+    
+window.addEventListener("storage", function(e) {  
+    window.location.href = "login.html"
+});
+
+
